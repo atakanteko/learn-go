896688571cfd8065e713a3d214bde8841181b4b1
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-05 01:43:42 +0300

    reading and writing json

--- main.go
+++ main.go
@@ -1,27 +1,66 @@
 package main
 
 import (
-	"github.com/atakanteko/learngo/helpers"
+	"encoding/json"
+	"fmt"
 	"log"
 )
 
-const numPool = 10
-
-func CalculateValue(intChannel chan int) {
-	randomNumber := helpers.RandomNumber(numPool)
-	intChannel <- randomNumber
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	HairColor string `json:"hair_color"`
+	HasDog    bool   `json:"has_dog"`
 }
+
 func main() {
-	var myVar helpers.SomeType
-	myVar.TypeName = "Some Name"
-	myVar.TypeNumber = 12
+	myJson := `
+[
+    {
+        "first_name": "Clark",
+        "last_name": "Kent",
+        "hair_color": "black",
+        "has_dog": true
+    },
+    {
+        "first_name": "Bruce",
+        "last_name": "Wayne",
+        "hair_color": "black",
+        "has_dog": false
+    }
+]`
+	var unmarshalled []Person
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+	if err != nil {
+		log.Println("Error unmarshalling json", err)
+	}
+	log.Printf("unmarshalled: %v", unmarshalled)
 
-	log.Println(myVar)
+	//write json from a struct
+	var mySlice []Person
 
-	intChannel := make(chan int)
-	defer close(intChannel)
+	var marshall1 Person
+	marshall1.FirstName = "Carla"
+	marshall1.LastName = "Dayton"
+	marshall1.HairColor = "Brown"
+	marshall1.HasDog = false
 
-	go CalculateValue(intChannel)
-	rndNumber := <-intChannel
-	log.Println(rndNumber)
+	mySlice = append(mySlice, marshall1)
+
+	var marshall2 Person
+	marshall2.FirstName = "James"
+	marshall2.LastName = "Fusco"
+	marshall2.HairColor = "Black"
+	marshall2.HasDog = true
+
+	mySlice = append(mySlice, marshall2)
+
+	for _, item := range mySlice {
+		log.Println(item)
+	}
+	newJSon, err := json.MarshalIndent(mySlice, "", "    ")
+	if err != nil {
+		log.Println("error marshalling", err)
+	}
+	fmt.Print(string(newJSon))
 }
