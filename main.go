573befb650f897d2010ed25c9c94c3c65b8938ea
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-05 01:16:58 +0300

    channels

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 	"log"
 )
 
+const numPool = 10
+
+func CalculateValue(intChannel chan int) {
+	randomNumber := helpers.RandomNumber(numPool)
+	intChannel <- randomNumber
+}
 func main() {
 	var myVar helpers.SomeType
 	myVar.TypeName = "Some Name"
 	myVar.TypeNumber = 12
 
 	log.Println(myVar)
+
+	intChannel := make(chan int)
+	defer close(intChannel)
+
+	go CalculateValue(intChannel)
+	rndNumber := <-intChannel
+	log.Println(rndNumber)
 }
