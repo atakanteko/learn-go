94ee3228728c5aa4fa3119a2ade83849426e370f
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-05 00:54:14 +0300

    packages

--- main.go
+++ main.go
@@ -1,47 +1,14 @@
 package main
 
-import "fmt"
-
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+import (
+	"github.com/atakanteko/learngo/helpers"
+	"log"
+)
 
 func main() {
-	dog := Dog{"Samson", "German Shephered"}
-	PrintInfo(&dog)
+	var myVar helpers.SomeType
+	myVar.TypeName = "Some Name"
+	myVar.TypeNumber = 12
 
-	gorilla := Gorilla{"Mazzi", "Gray", 36}
-	PrintInfo(&gorilla)
-}
-
-func PrintInfo(a Animal) {
-	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
-}
-
-func (d *Dog) Says() string {
-	return "Woof"
-}
-
-func (d *Dog) NumberOfLegs() int {
-	return 4
-}
-
-func (d *Gorilla) Says() string {
-	return "Uggghhhh"
-}
-
-func (d *Gorilla) NumberOfLegs() int {
-	return 2
+	log.Println(myVar)
 }
