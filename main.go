0e9b1dcd3ab960430a4cc0b7b318817c4ef4e371
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-05 13:27:45 +0300

    create new endpoint

--- main.go
+++ main.go
@@ -2,26 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	sum, _ := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2: %d", sum))
-}
-
-func addValues(x int, y int) (int, error) {
-	var sum int
-	sum = x + y
-	return sum, nil
-}
-
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
-	_ = http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/", greet)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
 }
