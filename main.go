14cabffa98a9c9a785caee86a9e5570ca6db0452
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-05 12:46:57 +0300

    function and handlers

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum, _ := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2: %d", sum))
+}
+
+func addValues(x int, y int) (int, error) {
+	var sum int
+	sum = x + y
+	return sum, nil
+}
+
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		n, err := fmt.Fprintf(writer, "Hello,world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 	_ = http.ListenAndServe(":8090", nil)
 }
